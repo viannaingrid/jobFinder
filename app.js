2b33b73ba34076cd12ba6229bd1ca36c8421b848
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-03-11 09:15:29 -0300

    :construction: config db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
@@ -7,6 +8,8 @@ app.listen(PORT, function(){
     console.log(`O express está rodando na porta ${PORT}`);
 })
 
+
+// Rotas
 app.get('/', function(req, rest){
     rest.send("Está funcionando 3");
 })

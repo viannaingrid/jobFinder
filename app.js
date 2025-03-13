fc3e949b9666f58c9feabc8a0ba04f691fdc40fc
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-03-13 15:32:39 -0300

    :construction: configurando app.js e handle bars certo com o engine

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express    = require('express');
-const exphbs     = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app        = express();
 const path       = require('path');
 const db         = require('./db/connection');
-const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
 
-const PORT = 3000;
+const port = 3000;
 
-app.listen(PORT, function(){
-    console.log(`O express está rodando na porta ${PORT}`);
+app.listen(port, function(){
+    console.log(`o express está rodando na porta ${port}`);
 });
 
 //body parser 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyparser.urlencoded({ extended: false}));
 
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main' }));
+app.engine('handlebars', engine({defaultlayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // static folder
@@ -26,17 +26,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 db
     .authenticate()
     .then(() => {
-        console.log("Conectou ao banco com sucesso");
+        console.log("conectou ao banco com sucesso");
     })
     .catch(err => {
-        console.log("Ocorreu um erro ao conectar", err);
+        console.log("ocorreu um erro ao conectar", err);
     });
 
 
-// Rotas
+// rotas
 app.get('/', function(req, rest){
-    rest.send("Está funcionando");
+    rest.render('index');
 })
 
-//Rotas dos trabalhos(jobs)
+//rotas dos trabalhos(jobs)
 app.use('/jobs', require('./routes/routes.js'));
